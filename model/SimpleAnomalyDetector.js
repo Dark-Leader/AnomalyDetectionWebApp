29adb441aba3c7e906a572a80309a318ea61ab28
DarkLe3der <amnb9@walla.com>
2021-05-26 20:05:08 +0300

    fixed bugs and added a smallest circle package

--- model/SimpleAnomalyDetector.js
+++ model/SimpleAnomalyDetector.js
@@ -1,7 +1,6 @@
 const { AnomalyReport } = require('./AnomalyDetector.js');
 const { pearson, linear_reg, Point, dev1, distance } = require('./anomaly_detection_util.js');
 const {TimeSeries} = require('./timeseries.js');
-//const {findMinCircle, distance} = require('./minCircle');
 const enclosingCircle = require('smallest-enclosing-circle')
 
 class correlatedFeatures {
@@ -24,9 +23,43 @@ class SimpleAnomalyDetector {
         this.threshold = 0.9;
     }
 
+    findThreshold(points, line) {
+        let max = 0;
+        let size = points.length;
+        for (let i = 0; i < size; i++) {
+            let d = Math.abs(points[i].y - line.f(points[i].x));
+            if (d > max) {
+                max = d;
+            }
+        }
+        return max;
+    }
+
+    learnHelper(max_correlation, f1, f2, points, mode) {
+        if(max_correlation > this.threshold){
+            let c = new correlatedFeatures();
+            c.feature1=f1;
+            c.feature2=f2;
+            c.correlation=max_correlation;
+            c.lin_reg=linear_reg(points);
+            c.threshold=this.findThreshold(points ,c.lin_reg)*1.1; // 10% increase
+            this.cf.push(c);
+        }
+        else if (max_correlation > 0.5 && max_correlation < this.threshold && mode) {
+            let c = new correlatedFeatures();
+            let cl = enclosingCircle(points);
+            c.feature1=f1;
+            c.feature2=f2;
+            c.correlation=max_correlation;
+            c.threshold=cl.r*1.1; // 10% increase
+            c.x=cl.x;
+            c.y=cl.y;
+            this.cf.push(c);
+        }    
+    }
+
     learnNormal (ts, mode) {
         let atts = ts.getAttributes();
-        let copy = atts.slice();
         let size = atts.length;
         let vals = [];
         for (let i = 0; i < atts.length; i++) {
@@ -36,76 +69,50 @@ class SimpleAnomalyDetector {
                 vals[i].push(x[j]);
             }
         }
-        let before = [];
-        for (let c of this.cf) {
-            before.push(c.feature1);
-            before.push(c.feature2);
-        }
+        let found_before = [];
         for (let i = 0; i < size; i++) {
             let f1 = atts[i];
-            if (before.indexOf(f1) > -1) {
-                continue;
+            for (let c of this.cf) {
+                let first = c.feature1;
+                let second = c.feature2;
+                if (found_before.indexOf(first) == -1) {
+                    found_before.push(first);
+                }
+                if (found_before.indexOf(second) == -1) {
+                    found_before.push(second);
+                }
             }
-            if (copy.indexOf(f1) == -1) {
+            if (found_before.indexOf(f1) > -1) {
                 continue;
             }
             let max_correlation = 0;
             let jmax = 0;
             for (let j = i+1; j < size; j++) {
+                let fj = atts[j];
+                if (found_before.indexOf(fj) > -1) {
+                    continue;
+                }
                 let correlation = Math.abs(pearson(vals[i], vals[j]));
                     if (correlation > max_correlation) {
                         max_correlation = correlation;
                         jmax = j;
                     }
             }
-            if (max_correlation < 0.5) {
-                continue;
-            }
-            let f2 = atts[jmax];
-            if (before.indexOf(f2) > -1) {
-                continue;
-            }
-            if (copy.indexOf(f2) == -1) {
-                continue;
-            }
             let points = [];
+            let f2 = atts[jmax];
             for (let i = 0; i < ts.getRowSize(); i++) {
                 points.push(new Point(ts.getAttData(f1)[i], ts.getAttData(f2)[i]));
             }
-            let correlated = new correlatedFeatures();
-            if (mode) {
-                //let c = findMinCircle(points);
-                let c = enclosingCircle(points);
-                correlated.threshold = c.r;
-                correlated.x = c.x;
-                correlated.y = c.y;
-                correlated.radius = c.r;
-            } else if (max_correlation >= 0.9) {
-                let line = linear_reg(points);
-                let max_dev = 0;
-                for (let i = 0; i < ts.getRowSize(); i++) {
-                    let deviation = dev1(new Point(ts.getAttData(f1)[i], ts.getAttData(f2)[i]),line);
-                    if (deviation > max_dev) {
-                        max_dev = deviation;
-                    }
-                }
-                correlated.threshold = max_dev;
-                correlated.lin_reg = line;
-            } else if (!mode && max_correlation < 0.9) {
-                continue;
-            }
-            correlated.feature1 = f1;
-            correlated.feature2 = f2;
-            correlated.correlation = max_correlation;
-            this.cf.push(correlated);
-            let idx1 = copy.indexOf(f1);
-            copy.splice(idx1, 1);
-            let idx2 = copy.indexOf(f2);
-            copy.splice(idx2, 1);
+            this.learnHelper(max_correlation, f1, f2, points, mode);
         }
-        if (mode) {
-            this.learnNormal(ts, !mode);
-        }
+    }
+
+    regularAnomalus(x, y, c) {
+        return (Math.abs(y - c.lin_reg.f(x)) > c.threshold);
+    }
+
+    multiIsAnomalous(x, y, c, mode) {
+        return (c.correlation >= this.threshold && this.regularAnomalus(x,y,c)) || (mode && c.correlation > 0.5 && c.correlation < 0.9 && distance(new Point(c.x, c.y), new Point(x,y)) > c.threshold);
     }
 
     detect(ts, mode) {
@@ -115,22 +122,11 @@ class SimpleAnomalyDetector {
             return anomalys;
         }
         for (let correlated of this.cf) {
-            let maxDev = correlated.threshold * 1.1;
-            let first = data[correlated.feature1];
-            let second = data[correlated.feature2];
-            for (let i = 0; i < ts.getRowSize(); i++) {
-                let point = new Point(first[i], second[i]);
-                if (mode) {
-                    let center = new Point(correlated.x,correlated.y);
-                    let dis = distance(center, point);
-                    if (dis > maxDev) {
-                        anomalys.push(new AnomalyReport(correlated.feature1 + "-" + correlated.feature2, i+1));
-                    }
-                } else {
-                    let deviation = dev1(point, correlated.lin_reg);
-                    if (deviation > maxDev) {
-                        anomalys.push(new AnomalyReport(correlated.feature1 + "-" + correlated.feature2, i+1));
-                    }
+            let x = ts.getAttData(correlated.feature1);
+            let y = ts.getAttData(correlated.feature2);
+            for (let i = 0; i < x.length; i++) {
+                if (this.multiIsAnomalous(x[i], y[i], correlated, mode)) {
+                    anomalys.push(new AnomalyReport(correlated.feature1 + "-" + correlated.feature2, i+1));
                 }
             }
         }
@@ -147,49 +143,18 @@ module.exports = {
 let ts = new TimeSeries('train.csv');
 let ts2 = new TimeSeries('test.csv');
 let ad = new SimpleAnomalyDetector();
-let hybrid = true;
+let hybrid = false;
 ad.learnNormal(ts, hybrid);
 console.log("Correlated Features:");
 for (let c of ad.cf) {
     console.log(c.feature1 + "-" + c.feature2);
 }
 let res = ad.detect(ts2, hybrid);
+
 console.log();
 console.log("anomalys Features:");
 for (r of res) {
     console.log(r.desc + '\t' + r.time);
 }
 
-
-/*
-correct results:
-    for hybrid = false:
-
-    Correlated Features:
-    A-B
-    C-D
-
-    anomalys Features:
-    A-B     73
-    A-B     74
-    A-B     75
-    A-B     76
-    C-D     133
-    C-D     134
-    C-D     135
-
-    for hybrid = true:
-    
-    Correlated Features:
-    A-B
-    C-D
-    E-F
-
-    anomalys Features:
-    E-F     8
-    E-F     9
-    E-F     10
-    E-F     11
-
-*/
 
