10026d6d5ba54050b396b464f81f3d65b4f04dad
noamsal <noamsalomon01@gmail.com>
2021-05-27 14:21:51 +0300

    Noam-1.added API for http post(comment)2.added post function for POST

--- controller/expServer.js
+++ controller/expServer.js
@@ -1,15 +1,31 @@
-const express =require('express')
-const fileUpload= require('express-fileupload')
-const model= require('../model/check.js')
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const model = require('../model/check.js')
 const fs = require('fs')
-const {TimeSeries} = require('../model/timeseries')
-const {SimpleAnomalyDetector, correlatedFeatures} = require('../model/SimpleAnomalyDetector')
-const {AnomalyReport} = require('../model/AnomalyDetector')
+const { TimeSeries } = require('../model/timeseries')
+const { SimpleAnomalyDetector, correlatedFeatures } = require('../model/SimpleAnomalyDetector')
+const { AnomalyReport } = require('../model/AnomalyDetector')
+
+/**
+ * API for sending an http POST req.:
+ * algorithm type-
+ * --KEY--: algorithm_type.
+ * --VALUE--: Regression Algorithm / Hybrid Algorithm
+ * 
+ * train file-
+ * --KEY--: normal_file
+ * 
+ * test file-
+ * --KEY--: anomaly_file  
+ */
+
+//anomalies array of json objs. returned to client after client's post
+const anomalies = []; 
 
 
-const app=express()
+const app = express()
 app.use(express.urlencoded({
-    extended:false
+    extended: false
 }))
 
 
@@ -17,43 +33,96 @@ app.use(fileUpload())
 
 app.use(express.static('../view'))
 
-app.get("/",(req,res)=>{
-res.sendFile("./index.html")
+/* GET */
+
+app.get("/", (req, res) => {
+    res.sendFile("./index.html")
 })
-app.post("/detect",(req,res)=>{
-res.write('searching for ' +  req.body.algorithm_type +':\n' )
-var type_algo =  req.body.algorithm_type
-if(req.files){
-    var file =req.files.normal_file;
-    var result= file.data.toString();
-    fs.writeFileSync('../model/train.csv', result, function(err) { // created train.csv
-        if (err) {
-           return console.error(err);
+
+/* POST */
+
+app.post("/detect", (req, res) => {
+    res.write('searching for ' + req.body.algorithm_type + ':\n')
+    var type_algo = req.body.algorithm_type
+    if (req.files) {
+        var file = req.files.normal_file;
+        var result = file.data.toString();
+        fs.writeFileSync('../model/train.csv', result, function (err) { // created train.csv
+            if (err) {
+                return console.error(err);
+            }
+        });
+        var file2 = req.files.anomaly_file
+        var result2 = file2.data.toString()
+        fs.writeFileSync('../model/test.csv', result2, function (err) { // created test.csv
+            if (err) {
+                return console.error(err);
+            }
+        });
+        let detector = new SimpleAnomalyDetector(); // created detector
+        let ts1 = new TimeSeries("../model/train.csv");
+        let ts2 = new TimeSeries("../model/test.csv");
+        let mode = false;
+        if (type_algo == "Hybrid Algorithm") {
+            mode = true;
         }
-    });
-    var file2 =req.files.anomaly_file
-    var result2= file2.data.toString()
-    fs.writeFileSync('../model/test.csv', result2, function(err) { // created test.csv
-        if (err) {
-           return console.error(err);
+        detector.learnNormal(ts1, mode);
+        let reports = detector.detect(ts2, mode);
+        for (let report of reports) {
+            res.write(report.desc + '\t' + report.time + "\n");
         }
-    });
-    let detector = new SimpleAnomalyDetector(); // created detector
-    let ts1 = new TimeSeries("../model/train.csv");
-    let ts2 = new TimeSeries("../model/test.csv");
-    let mode = false;
-    if (type_algo == "Hybrid Algorithm") {
-        mode = true;
     }
-    detector.learnNormal(ts1, mode);
-    let reports = detector.detect(ts2, mode);
-    for (let report of reports) {
-        res.write(report.desc + '\t' + report.time + "\n");
-    }
-}
-res.end()
+    res.end()
 
 })
+
+
+//noam
+app.post("/", (req, res) => {
+    res.write('searching for ' + req.body.algorithm_type + ':\n')
+    var type_algo = req.body.algorithm_type
+    if (req.files) {
+        var file = req.files.normal_file;
+        var result = file.data.toString();
+        fs.writeFileSync('../model/train.csv', result, function (err) { // created train.csv
+            if (err) {
+                return console.error(err);
+            }
+        });
+        var file2 = req.files.anomaly_file
+        var result2 = file2.data.toString()
+        fs.writeFileSync('../model/test.csv', result2, function (err) { // created test.csv
+            if (err) {
+                return console.error(err);
+            }
+        });
+        let detector = new SimpleAnomalyDetector(); // created detector
+        let ts1 = new TimeSeries("../model/train.csv");
+        let ts2 = new TimeSeries("../model/test.csv");
+        let mode = false;
+        if (type_algo == "Hybrid Algorithm") {
+            mode = true;
+        }
+        detector.learnNormal(ts1, mode);
+        let reports = detector.detect(ts2, mode);
+        for (let report of reports) {
+            //noam:
+            const anomaly = {
+                desc: report.desc, time: report.time,
+            };
+            //console.log(anomaly);
+            anomalies.push(anomaly);
+            //noam~
+            //res.write(report.desc + '\t' + report.time + "\n");
+        }
+        //noam:
+        res.write(JSON.stringify(anomalies));
+        //noam~
+    }
+    res.end()
+
+})
+
 
 
 app.listen(8080)
